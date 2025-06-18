ce6590fb1bddf59172c67aea236460b00861d809
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2025-06-18 13:39:06 +0530

    Initial Commit

---
+++ MovieDemoApp/MovieDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDemoAppApp.swift
+//  MovieDemoApp
+//
+//  Created by Bhumika Patel on 18/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
